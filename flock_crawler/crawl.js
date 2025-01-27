df8bd33afb38d3a898ceee178d3959e5c2c5fe19
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-01-26 21:09:44 -0800

    based DB

--- flock_crawler/crawl.js
+++ flock_crawler/crawl.js
@@ -1,8 +1,7 @@
 import { parse } from 'node-html-parser';
+import { DatabaseSync } from 'node:sqlite';
 
-// https://transparency.flocksafety.com/flock-safety-admins
-// https://transparency.flocksafety.com/flock-safety-le-training
-// https://transparency.flocksafety.com/florida-le-flock-training
+
 
 // ' Flock City PD - Law Enforcement Sales Demo',
 // ' Flock LE Training - Old',
@@ -20,26 +19,54 @@ import { parse } from 'node-html-parser';
 // ' Flock Safety PD Test Org',
 // ' Florida LE Flock Training',
 
+let found = []
+let failed = []
 
-const response = await fetch("https://transparency.flocksafety.com/flock-safety-admins");
+const database = new DatabaseSync('./audit_db');
+
+const initDatabase = `
+CREATE TABLE IF NOT EXISTS departments (
+  dept_slug TEXT PRIMARY KEY,
+  flock_status INTEGER NOT NULL,
+  name TEXT NOT NULL UNIQUE,
+  last_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  camera_count INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS searches (
+  search_dept TEXT NOT NULL,
+  search_id TEXT PRIMARY KEY, 
+  user_id TEXT NOT NULL,
+  time DATETIME NOT NULL,
+  camera_count INTEGER,
+  reason TEXT,
+
+  FOREIGN KEY (search_dept) REFERENCES departments (dept_slug)
+);
+`;
+
+database.exec(initDatabase);
+
+
+const createDepartment = database.prepare(`
+    INSERT INTO departments (dept_slug, flock_status, name, camera_count)
+    VALUES (?, ?, ?, ?)
+    RETURNING last_updated
+`);
+
+
+const response = await fetch("https://transparency.flocksafety.com/el-cerrito-ca-pd");
 const text = await response.text();
 
 const DOM = parse(text);
 
 
-list_depts(DOM).forEach(dept => {
+const depts = list_depts(DOM);
+console.log(`Searching ${depts.length} depts...`)
+depts.forEach(dept => {
     // console.log(dept + " " + gen_slug(dept))
     test_URL(gen_slug(dept))
 })
-
-
-
-// const Assoc_Depts = DOM.querySelector("#usage > div:nth-child(4) > div > div.pl-0.col-10 > div:nth-child(2) > div").innerText.split(", ")
-// // console.log(text)
-// Assoc_Depts.forEach(dept => {
-//     console.log(dept)
-// });
-
 
 function gen_slug(dept) {
     return dept.replaceAll(" - ", " ").replaceAll(" ", "-")
@@ -59,11 +86,49 @@ function list_depts(DOM) {
     return depts
 }
 
+function get_num_searches(DOM) {
+    // console.log(1)
+    const labels = DOM.querySelectorAll(".label.col-12");
+    // console.log(labels)
+    let num = null
+    labels.forEach(label => {
+        if(label.innerText == "Searches in the last 30 days") {
+            num = label.parentNode.nextSibling.firstChild.innerText
+        }
+    });
+
+    return num
+}
+
+function get_audit(DOM) {
+    // console.log(1)
+    const labels = DOM.querySelectorAll(".label.col-12");
+    // console.log(labels)
+    let csv = null
+    labels.forEach(label => {
+        if(label.innerText == "Public Search Audit") {
+            csv = label.parentNode.nextSibling.firstChild.firstChild.href;
+        }
+    });
+
+    return csv
+}
+
 async function test_URL(slug){
+    if(found.includes(slug) || failed.includes(slug)) {
+        return
+    }
+
     const url = "https://transparency.flocksafety.com/" + slug
+
     const response = await fetch(url);
     if(response.status == 200) {
-        console.log("Found " + url)
+        const text = await response.text();
+        const DOM = parse(text)
+        found.push(slug)
+        // console.log(`Found ${slug}, ${get_num_searches(DOM)} searches, ${get_audit != null ? "✅" : "no audit"} ${url}`)
+    } else {
+        failed.push(slug)
     }
 }
 
