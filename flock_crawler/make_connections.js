ff170ca5d8a9aa6b4be0fb25ffea50badbbc44cb
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-03-05 00:09:46 -0800

    generated connection data

--- flock_crawler/make_connections.js
+++ flock_crawler/make_connections.js
@@ -7,8 +7,13 @@ const getActiveDepartments = database.prepare(`
     SELECT dept_slug, name FROM departments WHERE flock_status = 200
 `);
 
-const getSearchIds = database.prepare(`
-    SELECT search_id FROM searches
+const createConnection = database.prepare(`
+    INSERT INTO connections (connection_id, dept_a, dept_b)
+    VALUES (?, ?, ?)
+`);
+
+const getConnectionIds = database.prepare(`
+    SELECT connection_id FROM connections
 `);
 
 function list_depts(DOM) {
@@ -25,9 +30,11 @@ function list_depts(DOM) {
     return depts
 }
 
-let active_departments = getActiveDepartments.all()
+const active_departments = getActiveDepartments.all()
+const cached_connections = getConnectionIds.all()
 
 const active_names = active_departments.map(d => d.name)
+const cached_connection_ids = cached_connections.map(d => d.connection_id)
 
 for(const department of active_departments){
     const url = "https://transparency.flocksafety.com/" + department.dept_slug
@@ -47,11 +54,13 @@ for(const department of active_departments){
     let known = []
 
     depts.forEach(dept => {
-        if(active_names.includes(dept)) {
+        const x = (department.name + ":" + dept).replaceAll(" ", "");
+        if(!cached_connection_ids.includes(x) && active_names.includes(dept)) {
+            createConnection.run(x, department.name, dept);
             known.push(dept)
         }
     })
 
-    console.log(`${department.name} has ${known.length} known connections`)
+    console.log(`${department.name} added ${known.length} connections`)
 
 }
