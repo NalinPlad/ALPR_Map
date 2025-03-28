753f90367a9adec42f72434ce6d6942d26a147bd
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-03-28 13:00:39 -0700

    change site

--- WWW/astro.config.mjs
+++ WWW/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://flock.ajith.fyi',
   vite: {
     plugins: [tailwindcss()]
   }
